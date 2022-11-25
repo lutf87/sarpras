2d5c30cd0017e1958347ab8272f97356ab90f324
lutfi87 <luthfiarrahman007@gmail.com>
2022-11-25 20:53:57 +0700

    templating master

---
+++ resources/views/master.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Dashboard</title>
+
+  <!-- Stylesheet -->
+  @include('master-items.stylesheet')
+  <!-- Stylesheet -->
+  
+  
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="{{ asset('images/sarpras-logo.png') }}" alt="AdminLTELogo" height="60" width="60">
+  </div>
+
+  
+
+<!-- Javascrip -->
+@include('master-items.javascript')
+<!-- Javascrip -->
+</body>
+</html>
